8ce10596d1a8d27f60ba053b81a3ffdc1bd62b35
Jason Y <ysyoo@ucdavis.edu>
2025-12-29 21:17:40 -0800

    fixing github publishing issue by changing base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MyWebsite/'
+  base: 'https://yeosun02.github.io/MyWebsite/'
 })
 
